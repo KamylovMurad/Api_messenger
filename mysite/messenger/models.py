f0505323cfccc455d7414f9b7dbcd33b619462d8
Мурад Камылов <kamylov@bk.ru>
2023-09-25 02:19:03 +0400

    new model Message

--- mysite/messenger/models.py
+++ mysite/messenger/models.py
@@ -7,3 +7,11 @@ class UserToken(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     chat_id = models.IntegerField(unique=True,  null=True)
     token = models.UUIDField(default=uuid.uuid4, unique=True)
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+
